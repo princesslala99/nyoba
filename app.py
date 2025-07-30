c2d60c7991c169e70e10c2c376b86a3212761ff5
princesslala99 <naepriki@gmail.com>
2025-07-30 16:24:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def add_bg_from_local(image_file):
         unsafe_allow_html=True
     )
 
-add_bg_from_local("download/bg.avif")
+add_bg_from_local("image/bg.avif")
 
 # --- COVER & SIDEBAR MENU ---
 st.set_page_config(
